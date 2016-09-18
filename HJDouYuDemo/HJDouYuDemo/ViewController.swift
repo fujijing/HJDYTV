9930680a34b41d037dc59d13614214742368ccca
fujijing <360254702@qq.com>
2016-09-18 15:44:52 +0800

    new project

---
+++ HJDouYuDemo/HJDouYuDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HJDouYuDemo
+//
+//  Created by 黄静静 on 16/9/18.
+//  Copyright © 2016年 HJing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HJDouYuDemo/HJDouYuDemoUITests/HJDouYuDemoUITests.swift
@@ -0,0 +1,36 @@
+//
+//  HJDouYuDemoUITests.swift
+//  HJDouYuDemoUITests
+//
+//  Created by 黄静静 on 16/9/18.
+//  Copyright © 2016年 HJing. All rights reserved.
+//
+
+import XCTest
+
+class HJDouYuDemoUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
