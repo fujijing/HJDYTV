968318161ca8707ee61fbceda4aab524d37660a4
fujijing <360254702@qq.com>
2016-09-19 16:02:49 +0800

    首页

--- HJDouYuDemo/HJDouYuDemo/MainViewController.swift
+++ HJDouYuDemo/HJDouYuDemo/MainViewController.swift
@@ -13,7 +13,16 @@ class MainViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        addChildVC("Home")
+        addChildVC("Live")
+        addChildVC("Follow")
+        addChildVC("Mine")
     }
-
+    
+    private func addChildVC(storyName: String){
+        
+        let childVC = UIStoryboard(name: storyName, bundle: nil).instantiateInitialViewController()!
+        addChildViewController(childVC)
+        
+    }
 }
